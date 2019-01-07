b3884bfac739ce24ecdf9632f9b6e9f14b22d06f
scostils <>
2019-01-07 19:06:37 +0100

    Javascript refactoring - much simpler

--- assets/js/tech-tree.js
+++ assets/js/tech-tree.js
@@ -1,15 +1,7 @@
 'use strict';
 
-var treeP;
-var treeS;
-var treeE;
-var selected = 'all';
-var treeDataP;
-var treeDataS;
-var treeDataE;
-
-var configP = {
-    container: '#tech-tree-physics',
+var config = {
+    //container: '#tech-tree-',
     rootOrientation: 'WEST', // NORTH || EAST || WEST || SOUTH
     nodeAlign: 'TOP',
     hideRootNode: true,
@@ -26,54 +18,6 @@ var configP = {
     callback: {
         onTreeLoaded: function() {
             init_tooltips();
-
-            const observer = lozad();
-            observer.observe();
-		}
-    }
-};
-var configS = {
-    container: '#tech-tree-society',
-    rootOrientation: 'WEST', // NORTH || EAST || WEST || SOUTH
-    nodeAlign: 'TOP',
-    hideRootNode: true,
-    siblingSeparation: 20,
-    subTeeSeparation:  20,
-    scrollbar: 'resize',
-    connectors: {
-        type: 'step'
-    },
-    node: {
-        HTMLclass: 'tech',
-        collapsable: false
-    },
-    callback: {
-        onTreeLoaded: function() {
-            //init_tooltips();
-
-            const observer = lozad();
-            observer.observe();
-		}
-    }
-};
-var configE = {
-    container: '#tech-tree-engineering',
-    rootOrientation: 'WEST', // NORTH || EAST || WEST || SOUTH
-    nodeAlign: 'TOP',
-    hideRootNode: true,
-    siblingSeparation: 20,
-    subTeeSeparation:  20,
-    scrollbar: 'resize',
-    connectors: {
-        type: 'step'
-    },
-    node: {
-        HTMLclass: 'tech',
-        collapsable: false
-    },
-    callback: {
-        onTreeLoaded: function() {
-            //init_tooltips();
 
             const observer = lozad();
             observer.observe();
@@ -123,65 +67,25 @@ $(document).ready(function() {
     load_tree();
 });
 
-function _loadP() {
-    var root = {key: "root", tier:0};
-    root.children = [];
+function _load(jsonData) {
+    var container = "#tech-tree-" + jsonData.children[0].name;
+    var myconfig = {container: container};
+    $.extend(true, myconfig, config);
 
-    for(var i = 0; i < treeDataP.children.length; i++) {
-        if(treeDataP.children[i].name === selected || selected == 'all') {
-            root.children = root.children.concat(treeDataP.children[i].children);
-        }
-    }
-
-    treeP = new Treant({chart:configP, nodeStructure: root});
-}
-function _loadS() {
-    var root = {key: "root", tier:0};
-    root.children = [];
-
-    for(var i = 0; i < treeDataS.children.length; i++) {
-        if(treeDataS.children[i].name === selected || selected == 'all') {
-            root.children = root.children.concat(treeDataS.children[i].children);
-        }
-    }
-
-    treeS = new Treant({chart:configS, nodeStructure: root});
-}
-function _loadE() {
-    var root = {key: "root", tier:0};
-    root.children = [];
-
-    for(var i = 0; i < treeDataE.children.length; i++) {
-        if(treeDataE.children[i].name === selected || selected == 'all') {
-            root.children = root.children.concat(treeDataE.children[i].children);
-        }
-    }
-
-    treeE = new Treant({chart:configE, nodeStructure: root});
+    new Treant({chart:myconfig, nodeStructure: jsonData.children[0]});
 }
 
 function load_tree() {
-    if (treeP === undefined || true) {
-        $.getJSON('physics.json', function(jsonData) {
-            treeDataP = jsonData;
-            setup(treeDataP);
-            _loadP();
-        });
-        $.getJSON('society.json', function(jsonData) {
-            treeDataS = jsonData;
-            setup(treeDataS);
-            _loadS();
-        });
-        $.getJSON('engineering.json', function(jsonData) {
-            treeDataE = jsonData;
-            setup(treeDataE);
-            _loadE();
-        });
-
-    } else {
-        selected = $('#area_selected').val();
-        tree.destroy();
-        $('#tech-tree').removeAttr('style');
-        _load();
-    }
+    $.getJSON('physics.json', function(jsonData) {
+        setup(jsonData);
+        _load(jsonData);
+    });
+    $.getJSON('society.json', function(jsonData) {
+        setup(jsonData);
+        _load(jsonData);
+    });
+    $.getJSON('engineering.json', function(jsonData) {
+        setup(jsonData);
+        _load(jsonData);
+    });
 }
